7ee0cab314dd05fec31a291bf7134784ac083b8e
Cut0x <71967731+Cut0x@users.noreply.github.com>
2021-11-06 03:09:30 +0100

    Update SlashCreateCmds.js

--- Data/Action/SlashCreateCmds.js
+++ Data/Action/SlashCreateCmds.js
@@ -2,27 +2,10 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Permissions } = require("discord.js");
 
 async function SlashCreateCmds(client, guilds) {
-    const say = new SlashCommandBuilder()
-        .setName("say")
-        .setDescription("Commande permettant de faire parler le bot !")
-        //.setDefaultPermission(Permissions.FLAGS.ADMINISTRATOR)
-        .addStringOption(option => option
-            .setName("message")
-            .setDescription("Ajoute un message.")
-            .setRequired(true))
-    client.guilds.cache.get(guilds.id).commands.create(say);
-
-    const setimageon = new SlashCommandBuilder()
-        .setName("setimageon")
-        .setDescription("Commande permettant d'obliger les images sur un salon !")
-        //.setDefaultPermission(Permissions.FLAGS.ADMINISTRATOR)
-    client.guilds.cache.get(guilds.id).commands.create(setimageon);
-
-    const setimageoff = new SlashCommandBuilder()
-        .setName("setimageoff")
-        .setDescription("Commande permettant de désactiver l'obligation d'images sur un salon !")
-        //.setDefaultPermission(Permissions.FLAGS.ADMINISTRATOR)
-    client.guilds.cache.get(guilds.id).commands.create(setimageoff);
+    const ping = new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Commande pour voir la latence du bot !")
+    client.guilds.cache.get(guilds.id).commands.create(ping);
 };
 
 module.exports = { SlashCreateCmds };
