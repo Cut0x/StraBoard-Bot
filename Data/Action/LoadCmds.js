25bc2d12aadfda5761d57b8a76dc61f2633b749e
Cut0x <71967731+Cut0x@users.noreply.github.com>
2021-11-06 04:40:01 +0100

    Update LoadCmds.js

--- Data/Action/LoadCmds.js
+++ Data/Action/LoadCmds.js
@@ -1,7 +1,16 @@
 async function LoadCmd(client) {
     client.on("interactionCreate", async interaction => {
         if (interaction.commandName === "ping") {
-            module.exports = require("../src/Commandes/Ping").Ping(interaction);
+            module.exports = require("../src/Commandes/Ping").Ping(client, interaction);
+        }
+        if (interaction.commandName === "setstar") {
+            module.exports = require("../src/Commandes/SetStar").SetStar(client, interaction);
+        }
+        if (interaction.commandName === "setstarchannel") {
+            module.exports = require("../src/Commandes/SetStarChannel").SetStarChannel(client, interaction);
+        }
+        if (interaction.commandName === "setstarnumber") {
+            module.exports = require("../src/Commandes/SetStarNum").SetStarNum(client, interaction);
         }
     });
 };
