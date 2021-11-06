bc6e98475cb10a30392984f685969b6630de0ddc
Cut0x <71967731+Cut0x@users.noreply.github.com>
2021-11-06 02:58:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Créé par Cut0x -> https://cutox.tech/
+
+const { Client, Intents, Permissions, MessageEmbed } = require("discord.js");
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+const db = require("quick.db");
+const { prefix, token } = require("./Data/config");
+
+client.once("ready", () => {
+    console.log("[LOADING] Conexions en cours !");
+
+    client.guilds.cache.forEach(guilds => {
+        module.exports = require("./Data/Action/SlashCmds").SlashLoad(client, guilds);
+        
+        console.log(`-> "${guilds.name}" chargé !`);
+
+        console.log("[LOADED] Connecté sur " + client.user.username + "#" + client.user.discriminator + ".");
+    });
+});
+
+client.login(token);
+
+module.exports = require("./Data/Action/LoadCmds").LoadCmd(client);
