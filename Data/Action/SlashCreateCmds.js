9cb0206dac8e43e3182573a496fdc9d1380260f5
Cut0x <71967731+Cut0x@users.noreply.github.com>
2021-11-06 04:41:03 +0100

    Update SlashCreateCmds.js

--- Data/Action/SlashCreateCmds.js
+++ Data/Action/SlashCreateCmds.js
@@ -6,6 +6,29 @@ async function SlashCreateCmds(client, guilds) {
         .setName("ping")
         .setDescription("Commande pour voir la latence du bot !")
     client.guilds.cache.get(guilds.id).commands.create(ping);
+
+    const setstar = new SlashCommandBuilder()
+        .setName("setstar")
+        .setDescription("Commande permettant d'activer/de désactiver le système de starboard !")
+    client.guilds.cache.get(guilds.id).commands.create(setstar);
+
+    const setstarchannel = new SlashCommandBuilder()
+        .setName("setstarchannel")
+        .setDescription("Commande permettant de choisir le salon du système de starboard !")
+        .addChannelOption(option => option
+            .setName("salon")
+            .setDescription("Choisi un salon TEXTUEL")
+            .setRequired(true))
+    client.guilds.cache.get(guilds.id).commands.create(setstarchannel);
+
+    const setstarnumber = new SlashCommandBuilder()
+        .setName("setstarnumber")
+        .setDescription("Commande permettant de choisir une limite au système de starboard !")
+        .addNumberOption(option => option
+            .setName("nombre")
+            .setDescription("Choisi un nombre")
+            .setRequired(true))
+    client.guilds.cache.get(guilds.id).commands.create(setstarnumber);
 };
 
 module.exports = { SlashCreateCmds };
