43717f1917e2a817904d4204ceaa8319da3c59f8
Cut0x <71967731+Cut0x@users.noreply.github.com>
2021-11-06 03:06:28 +0100

    Update LoadCmds.js

--- Data/Action/LoadCmds.js
+++ Data/Action/LoadCmds.js
@@ -1,1 +1,17 @@
+async function LoadCmd(client) {
+    client.on("interactionCreate", async interaction => {
+        if (interaction.commandName === "say") {
+            module.exports = require("../src/Commandes/Say").Say(interaction);
+        }
 
+        if (interaction.commandName === "setimageon") {
+            module.exports = require("../src/Commandes/SetImageon").SetImageon(interaction);
+        }
+        
+        if (interaction.commandName === "setimageoff") {
+            module.exports = require("../src/Commandes/SetImageoff").SetImageoff(interaction);
+        }
+    });
+};
+
+module.exports = { LoadCmd };
